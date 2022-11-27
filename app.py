874ecc95e6289053b64cdb10bbf97efe151b9e5e
zakkiya <fnszakkiya@gmail.com>
2022-11-27 22:49:14 +0700

    test1

--- app.py
+++ app.py
@@ -5,6 +5,4 @@ st.title('DATA MINING')
 data = pd.read_csv('https://raw.githubusercontent.com/RagilSalsabil/datamining/gh-pages/drug200.csv')
 data
 
-with st.container():
-    with st.sidebar:
-        st.header('Ragil')
+st.sidebar.title('Home')
